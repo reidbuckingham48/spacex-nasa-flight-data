b62a876ca46e6563498a575a7c89520dc7dc631f
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-07-22 16:02:01 -0500

    edits to data.html & data.js to populate table with data

--- data.js
+++ data.js
@@ -1,28 +1,54 @@
 queryLaunches();
 function queryLaunches(){
     $.get("https://api.spacexdata.com/v4/launches").then(function(response) {
+        console.log(response)
         // for each flight in response array, creates an option in dropdown menu with flight num, date and time
         for (var i = 0; i < response.length; i++) {
             var dateString = moment.unix(response[i].date_unix).format("YYYY-MM-DD HH:mm");
             var dropdownOpt = $('<option>');
-            dropdownOpt.attr('class','flightBtn');
             dropdownOpt.text("Flight Number: " + response[i].flight_number + "  Date: " + dateString);
-            dropdownOpt.val(response[i].flight_number);
+            dropdownOpt.val(response[i].id);
             $('#flightNum').append(dropdownOpt);
         };
     })
 }
 
-// $('.flightBtn').click( function() {
-//     userChoice = $(this).val();
-//     console.log(userChoice);
-// })
+$('.submitBtn').click( function() {
+    userChoice = (flightNum.value);
+    console.log(userChoice)
+    //query launch data
+    $.get("https://api.spacexdata.com/v4/launches/" + userChoice).then(function(response) {
+        console.log(response)
 
-// var flightDetails = response[0].details;
-// var launchDate = response[0].launch_date_local;
-// var launchYear = response[0].launch_year;
-// var articleLink = response[0].links.article_link;
-// var launchSite = response[0].launch_site.site_name_long;
+       $('#flightNumber').empty();
+       $('#flightNumber').text(response.flight_number)
+
+       $('#name').empty();
+       $('#name').text(response.name)
+
+       $('#date').empty();
+       $('#date').text(moment.unix(response.date_unix).format("YYYY-MM-DD HH:mm"))
+
+       //query launchpad information for 'location' field
+       $.get("https://api.spacexdata.com/v4/launchpads/" + response.launchpad).then(function(response) {
+       $('#location').empty();
+       $('#location').text(response.full_name)
+        })
+
+       $('#details').empty();
+       $('#details').text(response.details)
+        //possibly add if, else for null values
+       $('#article').empty();
+       $('#article').html("<a href = '" + response.links.article + "'>" +response.links.article + "</a>"+ "<br>" +
+       "<a href = '" + response.links.webcast + "'>" +response.links.webcast + "</a>" )
+})
+})
+
+// var flightDetails = response[0].details; -
+// var launchDate = response[0].launch_date_local;-
+// var launchYear = response[0].launch_year; - could not find
+// var articleLink = response[0].links.article_link; - links.article
+// var launchSite = response[0].launch_site.site_name_long; - could not find
 // var flightDetailsText= $("<p>");
 // var launchDateText = $("<p>");
 // var launchYearText= $("<p>");
